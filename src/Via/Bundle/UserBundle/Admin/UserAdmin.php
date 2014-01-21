2682f81b62b8d0eacd7b142cc29db99acac209ac
dev-null-or-trash-bin <dev-null-or-trash-bin@gmail.com>
2014-01-21 14:58:35 +0100

    refactoring

--- src/Via/Bundle/UserBundle/Admin/UserAdmin.php
+++ src/Via/Bundle/UserBundle/Admin/UserAdmin.php
@@ -2,32 +2,31 @@
 namespace Via\Bundle\UserBundle\Admin;
 
 use Sonata\UserBundle\Admin\Model\UserAdmin as BaseUserAdmin;
-
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 
-use FOS\UserBundle\Model\UserManagerInterface;
-use Sonata\AdminBundle\Route\RouteCollection;
-
-
 class UserAdmin extends BaseUserAdmin
 {
-        protected function configureFormFields(FormMapper $formMapper)
-        {
-            parent::configureFormFields($formMapper);
-            
-            $formMapper->with('ViaEbay')
-            ->add('viaebay_username', null, array(
-            	
-            ))
-            ->add('viaebay_password', null, array(
-            	
-            ))
-            ->add('viaebay_token', null, array(
-            	
-            ))
-            ->end();
-        }
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        parent::configureFormFields($formMapper);
+        
+        $formMapper->with('ViaEbay')
+        ->add('viaebay_username', null, array(
+        	
+        ))
+        ->add('viaebay_password', null, array(
+        	
+        ))
+        ->add('viaebay_token', null, array(
+        	
+        ))
+        ->add('reset', 'checkbox', array(
+            'mapped' => false,
+            'required' => false
+        ))
+        ->end();
+    }
 }
